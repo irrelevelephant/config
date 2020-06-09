9aecb5c6397d072a38a2f265f49bfe5e163e9f27
irrelevelephant <tsmithtree@gmail.com>
2020-06-09 16:05:22 -0700

    config updates

---
+++ .touchpad.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+synclient ClickFinger3=2
+synclient RightButtonAreaLeft=701
+synclient RightButtonAreaTop=659
+synclient MiddleButtonAreaRight=700
+synclient MiddleButtonAreaLeft=400
+synclient MiddleButtonAreaTop=659
