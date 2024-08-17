298bcb793f3c1d90165f7fcff8bc8e1f25b010da
irrelevelephant <tsmithtree@gmail.com>
2024-08-17 15:06:56 -0700

    sync fixes

--- sync.sh
+++ sync.sh
@@ -8,16 +8,15 @@ fi
 declare -A paths=(
     ["~/.emacs.d/user/*.el"]="./.emacs.d/user/"
 
-    ["~/.config/gtk-3.0/"]="./.config/gtk-3.0/"
-    ["~/.config/kitty/"]="./.config/kitty/"
-    ["~/.config/swap/"]="./.config/sway/"
-    ["~/.config/waybar/"]="./.config/waybar/"
-    ["~/.config/wofi/"]="./.config/wofi/"
+    ["~/.config/gtk-3.0/*"]="./.config/gtk-3.0/"
+    ["~/.config/kitty/*"]="./.config/kitty/"
+    ["~/.config/sway/*"]="./.config/sway/"
+    ["~/.config/waybar/*"]="./.config/waybar/"
+    ["~/.config/wofi/*"]="./.config/wofi/"
 
     ["~/.bashrc"]="./.bashrc"
     ["~/.mg"]="./.mg"
     ["~/.gitconfig"]="./.gitconfig"
-
 )
 
 sync_direction="$1"
@@ -26,16 +25,27 @@ for src in "${!paths[@]}"; do
     src_expanded=$(eval echo "$src")
     dest_expanded=$(eval echo "${paths[$src]}")
 
+    # don't sync monitor config
+    if [[ "$src_expanded" == *"monitors.conf" ]]; then
+        if [ "$sync_direction" == "up" ]; then
+            echo "Creating empty monitors.conf in $dest_expanded (UP)"
+            > "./.config/sway/monitors.conf"
+        fi
+        continue
+    fi
+
     if [ "$sync_direction" == "up" ]; then
-        for file in $src_expanded; do
-            echo "Syncing $file to $dest_expanded (UP)"
-            rsync -av "$file" "$dest_expanded"
-        done
+        if [[ "$dest_expanded" == */ ]]; then
+            mkdir -p "$dest_expanded"
+        fi
+        echo "Syncing $src_expanded to $dest_expanded (UP)"
+        cp -r $src_expanded "$dest_expanded"
     else
-        for file in $dest_expanded; do
-            echo "Syncing $file to $src_expanded (DOWN)"
-            rsync -av "$file" "$src_expanded"
-        done
+        if [[ "$src_expanded" == */ ]]; then
+            mkdir -p "$(dirname "$src_expanded")"
+        fi
+        echo "Syncing $dest_expanded to $src_expanded (DOWN)"
+        cp -r $dest_expanded "$(dirname "$src_expanded")"
     fi
 done
 
